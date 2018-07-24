8ccc090fb64de264e8ac2c5b7f2a8ebf5166d279
lfyfly <410793635@qq.com>
2018-07-24 14:11:35 +0800

    add：image font loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,22 @@
 var webpackCommon = require('./webpack.common.js')
 const merge = require('webpack-merge')
-module.exports =  merge(webpackCommon,{
+module.exports = merge(webpackCommon, {
   mode: 'development', // 当mode值为'production'时，webpack-dev-server 变动刷新反应很慢
-  devtool:'cheap-module-eval-source-map',
-  devServer:{}
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {}
 })
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,32 @@
 var webpackCommon = require('./webpack.common.js')
 const merge = require('webpack-merge')
-module.exports =  merge(webpackCommon,{
+module.exports = merge(webpackCommon, {
   mode: 'production', // 当mode值为'production'时，webpack-dev-server 变动刷新反应很慢
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'static/img/[name]-[hash:7].[ext]',
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|svg|eot|ttf)\??.*$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: 'static/font/[name]-[hash:7].[ext]',
+            limit: 8192
+          }
+        }
+      }
+    ]
+  },
+
 })
