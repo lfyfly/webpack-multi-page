5a260cbc3d2c5ca4e920fa3187918bd8374fc233
lfyfly <410793635@qq.com>
2018-07-26 20:04:56 +0800

    add: cfg.dev.proxy

--- webpack.cfg.js
+++ webpack.cfg.js
@@ -3,12 +3,23 @@ module.exports = {
   // 其他模板引擎自己在loader里面配
   // ejs-loader配了，但是不生效，还是走的html-webpack-plugin渲染，不支持include
   // 每次对pages 和 js入口文件 进行修改是需要 重启webpack
-  dev:{
-    assetsPublicPath:'/',
+  dev: {
+    assetsPublicPath: '/',
+    proxy: {
+      "/index/1.html": {
+        target: "http://localhost:8080",
+        pathRewrite: {"/index/1.html" : "/index1.html"}
+      },
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin:true,
+        pathRewrite: {"^/api" : ""}
+      }
+    }
   },
-  build:{
-    assetsSubDirectory:'static',
-    productionSourceMap: true,
-    assetsPublicPath:'./'
+  build: {
+    assetsSubDirectory: 'static',
+    productionSourceMap: false,
+    assetsPublicPath: './' // 也可是cdn地址
   }
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -20,7 +20,9 @@ module.exports = (env, argv) => {
         },
       ]
     },
-    devServer: {}
+    devServer: {
+      proxy: cfg.dev.proxy
+    }
   })
 };
 
